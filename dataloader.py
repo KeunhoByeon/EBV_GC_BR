590a5c590731f5036ccc2ff8225fff24661e1c63
변근호 <bkh5922@kookmin.kr>
2022-10-16 01:11:16 +0900

    First Commit

---
+++ dataloader.py
@@ -0,0 +1,50 @@
+import cv2
+import imgaug.augmenters as iaa
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+
+from utils import resize_and_pad_image
+
+
+class EBVGCDataset(Dataset):
+    def __init__(self, samples, input_size: int = None, do_aug: bool = False):
+        self.input_size = input_size
+        self.do_aug = do_aug
+
+        self.mean = np.array([0.485, 0.456, 0.406], dtype=np.float32).reshape(1, 1, 3)
+        self.std = np.array([0.229, 0.224, 0.225], dtype=np.float32).reshape(1, 1, 3)
+
+        # Augmentation setting (Not yet implemented all)
+        self.affine_seq = iaa.Sequential([iaa.Sometimes(0.5, iaa.Fliplr(0.5)), iaa.Sometimes(0.5, iaa.Flipud(0.5)), ], random_order=True)
+        # self.color_seq = iaa.Sequential([
+        #     iaa.Sometimes(0.1, iaa.GaussianBlur(sigma=(0, 0.5))),
+        #     iaa.Sometimes(0.1, iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5)),
+        #     iaa.Sometimes(0.2, iaa.Multiply((0.8, 1.2), per_channel=0.2)),
+        #     iaa.Sometimes(0.2, iaa.MultiplyHue((0.8, 1.2))),
+        #     iaa.Sometimes(0.2, iaa.MultiplySaturation((0.8, 1.2))),
+        #     iaa.Sometimes(0.2, iaa.LogContrast((0.8, 1.2))),
+        # ], random_order=True)
+
+        self.samples = samples
+
+    def __getitem__(self, index):
+        img_path, gt = self.samples[index]
+
+        img = cv2.imread(img_path)
+        if self.input_size is not None:
+            img = resize_and_pad_image(img, target_size=(self.input_size, self.input_size), keep_ratio=True, padding=True)
+
+        if self.split == 'train' and self.do_aug:
+            img = self.affine_seq.augment_image(img)
+
+        img = img.astype(np.float32) / 255.
+        img = (img - self.mean) / self.std
+        img = img.transpose(2, 0, 1)
+        img = torch.from_numpy(img)
+        gt = torch.from_numpy(np.array(gt)).type(torch.LongTensor)
+
+        return img_path, img, gt
+
+    def __len__(self):
+        return len(self.samples)
---
+++ train.py
@@ -0,0 +1,161 @@
+import argparse
+import os
+import random
+import time
+
+import torch
+import torch.nn as nn
+from tqdm import tqdm
+
+from data_utils import prepare_gastric_EBV_data_json
+from dataloader import EBVGCDataset
+from logger import Logger
+from model import Classifier
+from utils import accuracy
+
+
+def train(epoch, model, criterion, optimizer, train_loader, logger=None):
+    model.train()
+
+    total_confusion_mat, confusion_mat = [[0, 0], [0, 0]], [[0, 0], [0, 0]]
+    num_progress, next_print = 0, args.print_freq
+    for i, (_, inputs, targets) in enumerate(train_loader):
+        if torch.cuda.is_available():
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+        output = model(inputs)
+        loss = criterion(output, targets)
+        acc = accuracy(output, targets)[0]
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        logger.add_history('total', {'loss': loss.item(), 'accuracy': acc})
+        logger.add_history('batch', {'loss': loss.item(), 'accuracy': acc})
+
+        # Confusion Matrix
+        _, preds = output.topk(1, 1, True, True)
+        for t, p in zip(targets, preds):
+            confusion_mat[int(t)][p[0]] += 1
+            total_confusion_mat[int(t)][p[0]] += 1
+
+        num_progress += len(inputs)
+        if num_progress >= next_print:
+            if logger is not None:
+                logger(history_key='batch', epoch=epoch, batch=num_progress, confusion_mat=confusion_mat, time=time.strftime('%Y%m%d%H%M%S'))
+            confusion_mat = [[0, 0], [0, 0]]
+            next_print += args.print_freq
+
+        del output, loss, acc
+
+    if logger is not None:
+        logger(history_key='total', epoch=epoch, confusion_mat=total_confusion_mat)
+
+
+def val(epoch, model, criterion, val_loader, logger=None):
+    model.eval()
+
+    with torch.no_grad():
+        confusion_mat = [[0, 0], [0, 0]]
+        for i, (_, inputs, targets) in tqdm(enumerate(val_loader), leave=False, desc='Validation {}'.format(epoch), total=len(val_loader)):
+            if torch.cuda.is_available():
+                inputs = inputs.cuda()
+                targets = targets.cuda()
+
+            output = model(inputs)
+            loss = criterion(output, targets)
+            acc = accuracy(output, targets)[0]
+
+            logger.add_history('total', {'loss': loss.item(), 'accuracy': acc})
+
+            # Confusion Matrix
+            _, preds = output.topk(1, 1, True, True)
+            for t, p in zip(targets, preds):
+                confusion_mat[int(t)][p[0]] += 1
+
+            del output, loss, acc
+
+        if logger is not None:
+            logger('*Validation', history_key='total', confusion_mat=confusion_mat, time=time.strftime('%Y%m%d%H%M%S'))
+
+
+def run(args):
+    # Seed
+    if args.seed is not None:
+        random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        torch.backends.cudnn.deterministic = True
+
+    # Model
+    model = Classifier(args.model, num_classes=args.num_classes, pretrained=True)
+    if args.resume is not None:
+        model.load_state_dict(torch.load(args.resume))
+
+    # Criterion
+    criterion = nn.CrossEntropyLoss()
+
+    # Optimizer
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+
+    # CUDA
+    if torch.cuda.is_available():
+        model = model.cuda()
+        criterion = criterion.cuda()
+
+    # Dataset
+    train_set, val_set, test_set = prepare_gastric_EBV_data_json()
+    train_dataset = EBVGCDataset(train_set, input_size=args.input_size, do_aug=True)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True, shuffle=True)
+    val_dataset = EBVGCDataset(val_set, input_size=args.input_size, do_aug=False)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batch_size, num_workers=args.workers, pin_memory=True, shuffle=False)
+
+    # Logger
+    logger = Logger(os.path.join(args.result, 'log.txt'), epochs=args.epochs, dataset_size=len(train_loader.dataset), float_round=5)
+    logger.set_sort(['loss', 'accuracy', 'confusion_mat', 'time'])
+    logger(str(args))
+
+    save_dir = os.path.join(args.result, 'models')
+    os.makedirs(save_dir, exist_ok=True)
+
+    # Run training
+    print('Training...')
+    for epoch in range(args.start_epoch, args.epochs):
+        train(epoch, model, criterion, optimizer, train_loader, logger=logger)
+        if epoch % args.val_freq == 0:
+            val(epoch, model, criterion, val_loader, logger=logger)
+            torch.save(model.state_dict(), os.path.join(save_dir, '{}.pth'.format(epoch)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch Training')
+    # Model Arguments
+    parser.add_argument('--model', default='efficientnet_b0')
+    parser.add_argument('--num_classes', default=3, type=int, help='number of classes')
+    parser.add_argument('--pretrained', default=True, action='store_true', help='Load pretrained model.')
+    # Data Arguments
+    parser.add_argument('--data', default='~/data', help='path to dataset')
+    parser.add_argument('--workers', default=4, type=int, help='number of data loading workers')
+    parser.add_argument('--input_size', default=512, type=int, help='image input size')
+    # Training Arguments
+    parser.add_argument('--start_epoch', default=0, type=int, help='manual epoch number')
+    parser.add_argument('--epochs', default=100, type=int, help='number of total epochs to run')
+    parser.add_argument('--batch_size', default=16, type=int, help='mini-batch size')
+    parser.add_argument('--lr', default=0.0002, type=float, help='initial learning rate', dest='lr')
+    parser.add_argument('--seed', default=103, type=int, help='seed for initializing training.')
+    # Validation and Debugging Arguments
+    parser.add_argument('--val_freq', default=1, type=int, help='validation freq')
+    parser.add_argument('--print_freq', default=100, type=int, help='print and save frequency')
+    parser.add_argument('--result', default='results', help='path to results')
+    parser.add_argument('--resume', default=None, type=str, help='path to latest checkpoint')
+    parser.add_argument('--debug', default=False, action='store_true', help='debug validation')
+    args = parser.parse_args()
+
+    # Paths setting
+    args.data = os.path.expanduser(args.data)
+    args.result = os.path.expanduser(args.result)
+    args.result = os.path.join(args.result, time.strftime('%Y%m%d%H%M%S', time.localtime(time.time())))
+    os.makedirs(args.result, exist_ok=True)
+
+    run(args)
